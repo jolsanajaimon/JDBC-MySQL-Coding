93a1a7dfc5793e94c0d870a7c157c6193319f607
JOLSANA JAIMON <71826232+jolsanajaimon@users.noreply.github.com>
2024-06-26 21:53:19 +0530

    Add files via upload

---
+++ JDBC/student_databases.sql
@@ -0,0 +1,7 @@
+create database student_databases;
+use student_databases;
+create table students(Student_id int auto_increment primary key,Student_name 
+varchar(50),Student_place varchar(50),course varchar(50),contact_num varchar(15));
+show tables;
+describe students;
+select * from students;
